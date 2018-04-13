a9198fac575d27e941f41305e995fa351599855a
feshout <maciej.ukowski@gmail.com>
2018-04-13 15:06:26 +0200

    Add main class app controller

--- src/main/java/com/codecool/networking/NetChat.java
+++ src/main/java/com/codecool/networking/NetChat.java
@@ -1,7 +1,32 @@
 package com.codecool.networking;
 
+import com.codecool.networking.modes.Client;
+import com.codecool.networking.modes.Server;
+
+import java.util.Scanner;
+
 public class NetChat {
     public static void main(String[] args) {
 
+
+        if(args.length == 1){
+            String name = getName();
+            Server server = new Server(Integer.parseInt(args[0]), name);
+            server.start();
+        }
+        if(args.length == 2){
+            String name = getName();
+            Client client = new Client(args[0], Integer.parseInt(args[1]), name);
+            client.start();
+        }
+
+    }
+
+    private static String getName() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What's your name: ");
+        String name = scanner.nextLine();
+        return name;
     }
 }
